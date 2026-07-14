4199056a54af69862ec56e99dd55527274a59975
Balaji Bondar <balajibondar@gmail.com>
2026-07-14 18:39:10 +0530

    Initial commit

---
+++ tests/community-case-creation.spec.ts
@@ -0,0 +1,114 @@
+import { test, expect } from "@playwright/test";
+const { readFileSync } = require("fs");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+let execEnv = { env: { ...process.env, FORCE_COLOR: "0" } };
+
+let testDataJSON = "tests/test-data/community-case-creation-data.json";
+
+let SiteName = "MIAW_Experiencce_Site";
+let OrgName = "devhub";
+let clearTestDataBefore = true;
+let clearTestDataAfter = true;
+
+test("TS#001-create-community-case", async ({ page }) => {
+  test.slow();
+
+  //Load Test Data
+  let testData = readFileSync(testDataJSON);
+  var testDataObj = JSON.parse(testData);
+
+  // clear test data before
+  if (clearTestDataBefore) {
+    try {
+      console.log(">> Clearing test data before.");
+      let sfOutput = await exec(
+        "sf data:delete:record --json -s Case --where \"subject='" +
+          testDataObj.subject +
+          "'\"",
+        execEnv
+      );
+      var jsonObj = JSON.parse(sfOutput.stdout.trim());
+      console.log(">> Before clear data status: " + jsonObj.result.success);
+    } catch (error) {
+      console.log(">> No matching record found.");
+    }
+  }
+
+  // Get SF Site URL
+  let sfOutput = await exec(
+    "sf data:query --json -q \"SELECT Id, GuestUserId FROM Site WHERE Name = '" +
+      SiteName +
+      "'\"",
+    execEnv
+  );
+  var jsonObj = JSON.parse(sfOutput.stdout.trim());
+  let SF_SITE_ID = jsonObj.result.records[0].Id;
+  console.log(">> SF_SITE_ID " + SF_SITE_ID);
+
+  sfOutput = await exec(
+    "sf data:query --json -q \"SELECT SecureURL FROM SiteDetail WHERE DurableId = '" +
+      SF_SITE_ID +
+      "'\"",
+    execEnv
+  );
+  jsonObj = JSON.parse(sfOutput.stdout.trim());
+  let SF_SITE_URL = jsonObj.result.records[0].SecureUrl;
+  console.log(">> SF_SITE_URL " + SF_SITE_URL);
+
+  //await page.goto("https://orgfarm-c52be2a542-dev-ed.develop.my.site.com/s/");
+  await page.goto(SF_SITE_URL);
+
+  await page.getByRole("button", { name: "Contact Support" }).click();
+
+  await page.getByRole("textbox", { name: "Subject" }).click();
+  await page
+    .getByRole("textbox", { name: "Subject" })
+    .fill(testDataObj.subject);
+
+  await page.getByRole("textbox", { name: "Description" }).click();
+  await page
+    .getByRole("textbox", { name: "Description" })
+    .fill(testDataObj.description);
+
+  await page.getByRole("textbox", { name: "Web Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Web Email" })
+    .fill(testDataObj.webEmail);
+
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page.locator("h1")).toContainText("Your case was created.");
+
+  // Query SF and get case subject.
+  sfOutput = await exec(
+    "sf data:query --json -q \"SELECT Id,Subject FROM Case WHERE Subject='" +
+      testDataObj.subject +
+      "' ORDER BY CreatedDate DESC LIMIT 1\"",
+    execEnv
+  );
+
+  jsonObj = JSON.parse(sfOutput.stdout.trim());
+  let CaseSubject = jsonObj.result.records[0].Subject;
+  console.log(">> Case subject is: " + CaseSubject);
+
+  // Verify case subject
+  expect(CaseSubject).toBe(testDataObj.subject);
+
+  // clear test data before
+  if (clearTestDataAfter) {
+    try {
+      console.log(">> Clearing test data After.");
+      let sfOutput = await exec(
+        "sf data:delete:record --json -s Case --where \"subject='" +
+          testDataObj.subject +
+          "'\"",
+        execEnv
+      );
+      var jsonObj = JSON.parse(sfOutput.stdout.trim());
+      console.log(">> After clear data status: " + jsonObj.result.success);
+    } catch (error) {
+      console.log(">> No matching record found.");
+    }
+  }
+});
---
+++ tests/create-new-account.spec.ts
@@ -0,0 +1,96 @@
+import { test, expect } from "@playwright/test";
+const { readFileSync } = require("fs");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+let execEnv = { env: { ...process.env, FORCE_COLOR: "0" } };
+
+let testDataJSON = "tests/test-data/create-new-account-data.json";
+
+let OrgName = "agentforce-org";
+let clearTestDataBefore = true;
+let clearTestDataAfter = true;
+
+test("TS#001-create-new-account", async ({ page }) => {
+  test.slow();
+
+  //Load Test Data
+  let testData = readFileSync(testDataJSON);
+  var testDataObj = JSON.parse(testData);
+
+  // clear test data before
+  if (clearTestDataBefore) {
+    try {
+      console.log(">> Clearing test data before...");
+      let sfOutput = await exec(
+        "sf data:delete:record --json -s Account --where \"Name='" +
+          testDataObj.Name +
+          "'\"",
+        execEnv
+      );
+      var jsonObj = JSON.parse(sfOutput.stdout.trim());
+      console.log(">> Before clear data status: " + jsonObj.result.success);
+    } catch (error) {
+      console.log(">> No matching record found...");
+    }
+  }
+
+  // Get SF pre-authenticated URL
+  let sfOutput = await exec(
+    "sf org:open -o " +
+      OrgName +
+      " --path /lightning/page/home --url-only --json",
+    execEnv
+  );
+  var jsonObj = JSON.parse(sfOutput.stdout.trim());
+  let SF_URL = jsonObj.result.url;
+  console.log(">> SF_URL " + SF_URL);
+
+  // start test here
+  await page.goto(SF_URL);
+
+  await page.getByRole("link", { name: "Accounts" }).click();
+  await page.getByRole("button", { name: "New", exact: true }).click();
+  await page.getByRole("textbox", { name: "Account Name" }).click();
+  await page
+    .getByRole("textbox", { name: "Account Name" })
+    .fill(testDataObj.Name);
+  await page.getByRole("button", { name: "Save", exact: true }).click();
+
+  // Assert result
+  await page.waitForURL("**/view");
+  await page.waitForSelector("records-highlights2");
+  await expect(page.locator("records-highlights2")).toContainText(
+    testDataObj.Name
+  );
+
+  // Get record Id from URL
+  let url = new URL(page.url());
+  let path_split = url.pathname.split("/");
+  console.log(">> path_split " + path_split);
+
+  // Assert URL
+  expect(path_split[3]).toBe("Account");
+  expect(path_split[5]).toBe("view");
+  let AccountId = path_split[4];
+  console.log(">> AccountId: " + AccountId);
+
+  // clear test data after
+  if (clearTestDataAfter) {
+    try {
+      console.log(">> Clearing test data after...");
+      let sfOutput = await exec(
+        "sf data:delete:record --json -s Account --where \"Name='" +
+          testDataObj.Name +
+          "'\"",
+        execEnv
+      );
+      var jsonObj = JSON.parse(sfOutput.stdout.trim());
+      console.log(">> After clear data status: " + jsonObj.result.success);
+    } catch (error) {
+      console.log(">> No matching record found...");
+    }
+  }
+
+  // ** Test has passed!
+  console.log(">> " + test.info().title + ": Test Passed!");
+});
